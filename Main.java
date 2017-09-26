0491cfb527ed763d65bf070b494e828864c161a3
Leo <helloleolin@gmail.com>
2017-09-25 20:06:02 -0700

    Restructured PhonicDictionary class for clearer responsibility separations

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
 package com.leo;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        String fats = "there's fifty fats.";
-        for(int i = 0; i < fats.length(); i++){
-            System.out.println(fats.charAt(i));
-        }
-
+        RhymeProvider rp = new RhymeProvider("TestSounds.txt");
+        rp.initialize();
+        ArrayList<String> fats = rp.findRhyme("apple");
+        System.out.println(fats.size());
+        System.out.println(fats.get(0));
     }
 }
---
+++ PhonicDictionary.java
@@ -0,0 +1,137 @@
+package com.leo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * Created by Lelily on 9/22/2017.
+ */
+
+public class PhonicDictionary {
+    private HashMap<String, String> wordBank;  /*k: phonics, v: word*/
+    private HashMap<String, String> lastSoundsBank;  /*k: word, v: last sounds*/
+    private HashSet<String> vowelBank; /*All possible vowel sounds*/
+    private String file_path;
+
+
+    //Constructor will initialize all variable fields
+    PhonicDictionary(String file_path) {
+        this.file_path = file_path;
+        this.wordBank = new HashMap<>();
+        this.lastSoundsBank = new HashMap<>();
+        this.vowelBank = new HashSet<>();
+    }
+
+
+    //This will populate the 2 hash maps and the vowel bank hash set
+    public void initialize() {
+        FileProcessor processor = new FileProcessor();
+        fillWordBank(processor);
+        fillVowelBank(processor);
+        fillLastSoundsBank(wordBank);
+    }
+
+
+    //The word bank will be populated by words from the path in the input
+    private void fillWordBank(FileProcessor p) {
+        this.wordBank = p.pullWords(file_path);
+    }
+
+
+    //Vowels will be populated with all available english vowel sounds
+    private void fillVowelBank(FileProcessor p) {
+        this.vowelBank = p.pullVowels("VowelBank.txt");
+    }
+
+
+    /*
+     Populate the Last Sounds Bank
+     For every word in the full phonic word bank:
+     - Find the last sound from the phonics
+     - Add the word(k) with the last sound to the last sounds word bank
+      */
+    private void fillLastSoundsBank(HashMap<String, String> wordBank) {
+        wordBank.forEach((String phonics, String word) -> {
+            String lastSound = findLastSound(phonics);
+            lastSoundsBank.put(word, lastSound);
+        });
+    }
+
+
+    /*
+    From a string representing the complete phonics of a word
+    Split the phonics into individual phonic unit and store it in an array
+    First find the last vowel in the word
+    Then save that last vowel and any consonants that follows as a string
+    with each phonic unit separated by a whitespace and concatenate them
+     */
+    private String findLastSound(String phonics) {
+        String[] phonicsArr = phonics.split(" ");
+        int lastVowel = findLastVowel(phonicsArr);
+        String lastSound = putTogether(phonicsArr, lastVowel);
+        return lastSound;
+    }
+
+
+    /*
+    Given an array of phonic units, starting from the end, finds
+    the last occurring vowel using the vowel bank hash set
+     */
+    private int findLastVowel(String[] arr) {
+        int vowelInd = -1, last = arr.length - 1;
+        for (int i = last; i >= 0; i--) {
+            if ((vowelBank.contains(arr[i]))) {
+                vowelInd = i;
+                break;
+            }
+        }
+        return vowelInd;
+    }
+
+
+    //Concatenates the array of phonic units into a single string
+    private String putTogether(String[] arr, int index) {
+        String lastSound = "";
+        int end = arr.length - 1;
+        for (int i = index; i < end; i++) {
+            lastSound = lastSound.concat(arr[i]).concat(" ");
+        }
+        lastSound = lastSound.concat(arr[end]);
+        return lastSound;
+    }
+
+
+    //If the Last Sounds Bank does not contain the word, returns true
+    public boolean doesNotContain(String word) {
+        return !(lastSoundsBank.containsKey(word));
+    }
+
+
+    //Finds the last sound for a word
+    public String idLastSound(String word) {
+        return lastSoundsBank.get(word);
+    }
+
+
+    /*
+       Populates the hash map Rhyme Bank (rb)
+       This method will go through every entry in the last sound word bank (lsb):
+           -If the rb has the last sound in the lsb entry as a key, then add the word to
+            the array list of words in the rb
+           -If the rb does not have the last sound in the lsb entry as a key, then
+            initialize a new array list of words, add the lsb's word into the list, and
+            put the last sound and the list into rb
+        */
+    public void populateRhymeBank(HashMap<String, ArrayList<String>> rhymeBank){
+        lastSoundsBank.forEach((String word, String lastSound) -> {
+            if (rhymeBank.containsKey(lastSound)) {
+                rhymeBank.get(lastSound).add(word);
+            } else {
+                ArrayList<String> rhymeWords = new ArrayList<>();
+                rhymeWords.add(word);
+                rhymeBank.put(lastSound, rhymeWords);
+            }
+        });
+    }
+}
