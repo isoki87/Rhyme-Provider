375b34bf9fba59da9acb0b262fd6167453d50293
isoki87 <helloleolin@gmail.com>
2018-05-02 16:33:02 -0700

    4-space tabulation to 2-space

--- Main.java
+++ Main.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) {
-        RhymeProvider rp = new RhymeProvider("TestSounds.txt");
-        rp.initialize();
-        ArrayList<String> test = rp.findRhyme("apple");
-        System.out.println(test.size());
-        System.out.println(test.get(0));
-    }
+  public static void main(String[] args) {
+    RhymeProvider rp = new RhymeProvider("TestSounds.txt");
+    rp.initialize();
+    ArrayList<String> test = rp.findRhyme("apple");
+    System.out.println(test.size());
+    System.out.println(test.get(0));
+  }
+
 }
