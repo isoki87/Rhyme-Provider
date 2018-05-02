9a8952828bc699344be7cbfb4f69c9f1f9d7d29e
isoki87 <helloleolin@gmail.com>
2018-05-02 16:32:15 -0700

    4-space tabulation to 2-space for readability

--- FileProcessor.java
+++ FileProcessor.java
@@ -9,23 +9,23 @@ import java.util.ArrayList;
  * Created by Lelily on 9/22/2017.
  */
 public class FileProcessor {
-    private FileReader fr;
-    private BufferedReader reader;
-    private String file_path;
+  private FileReader fr;
+  private BufferedReader reader;
+  private String file_path;
 
-    FileProcessor(String file_path) throws IOException{
-        this.file_path = file_path;
-        this.fr = new FileReader(this.file_path);
-        this.reader = new BufferedReader(fr);
-    }
+  FileProcessor(String file_path) throws IOException{
+    this.file_path = file_path;
+    this.fr = new FileReader(this.file_path);
+    this.reader = new BufferedReader(fr);
+  }
 
-    public ArrayList<String> returnLines() throws IOException {
-        String line;
-        ArrayList<String> allLines = new ArrayList<>();
-        while((line = reader.readLine()) != null){
-            allLines.add(line);
-        }
-        reader.close();
-        return allLines;
+  public ArrayList<String> returnLines() throws IOException {
+    String line;
+    ArrayList<String> allLines = new ArrayList<>();
+    while((line = reader.readLine()) != null){
+        allLines.add(line);
     }
+    reader.close();
+    return allLines;
+  }
 }
