c37a012016a0bc7754adcedff8bd3c81497fdd72
isoki87 <helloleolin@gmail.com>
2018-05-22 11:51:29 -0700

    Deployed on helloleolin.com as of 5/21/2018

--- FileProcessor.java
+++ FileProcessor.java
@@ -1,4 +1,4 @@
-package com.leo;
+package com.leo.rhyme;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -9,23 +9,28 @@ import java.util.ArrayList;
  * Created by Lelily on 9/22/2017.
  */
 public class FileProcessor {
-  private FileReader fr;
-  private BufferedReader reader;
-  private String file_path;
+	//private InputStream input;
+	//private InputStreamReader isr;
+    private FileReader fr;
+    private BufferedReader reader;
+    private String file_path;
 
-  FileProcessor(String file_path) throws IOException{
-    this.file_path = file_path;
-    this.fr = new FileReader(this.file_path);
-    this.reader = new BufferedReader(fr);
-  }
-
-  public ArrayList<String> returnLines() throws IOException {
-    String line;
-    ArrayList<String> allLines = new ArrayList<>();
-    while((line = reader.readLine()) != null){
-        allLines.add(line);
+    FileProcessor(String file_path) throws IOException{
+        this.file_path = file_path;
+        //this.input = this.getClass().getResourceAsStream(this.file_path);
+        //this.isr = new InputStreamReader(input);
+        //this.reader = new BufferedReader(isr);
+        this.fr = new FileReader(this.file_path);
+        this.reader = new BufferedReader(fr);       
     }
-    reader.close();
-    return allLines;
-  }
+
+    public ArrayList<String> returnLines() throws IOException {
+        String line;
+        ArrayList<String> allLines = new ArrayList<>();
+        while((line = reader.readLine()) != null){
+            allLines.add(line);
+        }
+        reader.close();
+        return allLines;
+    }
 }
